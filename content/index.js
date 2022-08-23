7a6cdde35f30b4de1b75e6fd818e8d82945c355b
Jeremy Walker (Kerwal) <jeremy@kerwal.com>
2022-08-22 21:19:33 -0700

    moved bootstrap init into requirejs init

--- content/index.js
+++ content/index.js
@@ -2,10 +2,23 @@ require.config({
     paths: {
         moment: 'http://momentjs.com/downloads/moment.min',
         tabulator: 'https://unpkg.com/tabulator-tables@5.2.3/dist/js/tabulator.min',
+        bootstrap: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min',
+    },
+    onNodeCreated: function(node, config, module, path) {
+        // Here's  alist of differet integrities for different scripts
+        // Append to this list for all scripts that you want SRI for
+        let sri = {
+            "bootstrap": "sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa",
+        };
+
+        if (sri[module]) {
+            node.setAttribute('integrity', sri[module]);
+            node.setAttribute('crossorigin', 'anonymous');
+        }
     }
 });
 
-requirejs(['tabulator', 'moment', 'table-config'], function (Tabulator, moment, table_config) {
+requirejs(['tabulator', 'bootstrap', 'table-config'], function (Tabulator, bootstrap, table_config) {
     if(!table_config) table_config = {};
     // override ajaxURL
     table_config.ajaxURL = '/table-data';
