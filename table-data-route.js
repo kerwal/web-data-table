0ac52f2417bc85208f76ac70a4f8d0bdcd59084b
Jeremy Walker, Lincoln USD <jwalker@lusd.net>
2022-08-24 16:07:18 -0700

    - added post and delete methods - removed use of JSONPath - fixed default value of table_data

--- table-data-route.js
+++ table-data-route.js
@@ -3,17 +3,17 @@ module.exports = (config = {}) => {
   const express = require('express');
   var router = express.Router();
 
-  if(!config.table_data_path) return router;
+  if (!config.table_data_path) return router;
 
   const fs = require('node:fs');
-  const { JSONPath } = require('jsonpath-plus');
+  // const { JSONPath } = require('jsonpath-plus');
 
   // store the table's data in memory as a cache
   // keep track of changes to the table
   // write the cache to file every second, but only if there have been changes
 
   // load the file
-  var table_data = {};
+  var table_data = [];
 
   try {
     table_data = JSON.parse(fs.readFileSync(config.table_data_path));
@@ -43,34 +43,86 @@ module.exports = (config = {}) => {
   // start the save schedule after one second
   setTimeout(save_cache, 1000);
 
+  // R - READ (table)
   router.get('/', function (req, res, next) {
     // tha page requested a fresh copy of the table
-    // return a copy of the cache
+    // return a copy from the cache
     res.json(table_data);
   });
 
+  // U - UPDATE (cell)
   router.patch('/', function (req, res, next) {
-    // the page requested a change to the table
-    // req.body.jsonpath=...
+    // the page requested an atomic change to the table
+    // req.body.id=...
+    // req.body.field=...
     // req.body.value=...
+
     // pass the change to JSONPath on the cached table object to make the change
-    let result = { jsonpath: req.body.jsonpath, value: req.body.value, found: false, success: false };
-    JSONPath({
-      path: req.body.jsonpath, json: table_data, callback: (value, type, payload) => {
-        // console.log(value, type, payload);
+    // let result = { jsonpath: `$[?(@.id==${req.body.id})].${req.body.field}`, value: req.body.value, found: false, success: false };
+    // JSONPath({
+    //   path: result.jsonpath, json: table_data, callback: (value, type, payload) => {
+    //     // console.log(value, type, payload);
+    //     result.found = true;
+    //     if (type === 'value') {
+    //       payload.parent[payload.parentProperty] = req.body.value;
+    //       result.success = true;
+    //       // record the change requests
+    //       recent_changes.push(result);
+    //     }
+    //   }
+    // });
+
+    try {
+      let result = { ...req.body, found: false, success: false };
+      let found_row = table_data.find((row) => row.id === req.body.id);
+      if (found_row) {
         result.found = true;
-        if (type === 'value') {
-          payload.parent[payload.parentProperty] = req.body.value;
-          result.success = true;
-          // record the change requests
-          recent_changes.push({ jsonpath: req.body.jsonpath, value: req.body.value });
-        }
+        found_row[req.body.field] = req.body.value;
+        result.success = true;
       }
-    });
-    console.log(result);
-    // console.dir(table_data);
-    res.json(result);
+      console.log(result);
+      res.json(result);
+    } catch (error) {
+      res.json({ ...result, error });
+    }
   });
 
+  // C - CREATE (row)
+  router.post('/', function (req, res, next) {
+    // the page requested to add a row to the table
+    // the object in req.body is the definition of the new row
+    // if the body is falsy (including undefined) then make a fresh unpopulated row
+    if (!req.body) req.body = {};
+    let new_row = req.body;
+    try {
+      // assign the next sequential id to the row
+      // find the greatest id and add 1
+      new_row.id = table_data.reduce((id, row) => (row.id > id) ? row.id : id, -1) + 1;
+      console.log('new_row.id:', new_row.id);
+      // insert the row into the cache
+      table_data.push(new_row);
+      console.log(table_data);
+      // respond with success and the new_id field
+      res.json({ success: true, id: new_row.id });
+    } catch (error) {
+      res.json({ success: false, error });
+    }
+  });
+
+  // D - DELETE
+  router.delete('/', function (req, res, next) {
+    // the page requested to delete a row from the table
+    // the id of the row is in req.body.id
+    try {
+      let row_index = table_data.findIndex((row) => row.id === req.body.id);
+      if (row_index > -1)
+        table_data.splice(row_index, 1);
+      // respond with success even i f the row wasn't found
+      res.json({ success: true });
+    } catch (error) {
+      // respond with failure and the error
+      res.json({ success: false, error });
+    }
+  });
   return router;
 }
